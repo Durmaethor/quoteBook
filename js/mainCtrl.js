3101fae765f034b8d5ed2195d5a12c44969db350
Ryan Kramer <rkramer613@gmail.com>
2017-02-11 11:33:16 -0700

    Finished code entry, not 100% working

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -2,4 +2,19 @@ angular.module('quoteBook').controller('mainCtrl', function($scope, dataService)
 
     $scope.quotes = dataService.getQuotes();
 
+    $scope.deleteMe = function(textToDelete){
+        dataService.removeData(textToDelete);
+    }
+
+    $scope.addQuote = function(){
+        var newQuote = {
+            text:$scope.newQuoteText,
+            auther:$scope.newQuoteAuthor
+        }
+        if(dataService.addData(newQuote)){
+            $scope.newQuoteText = '';
+            $scope.newQuoteAuthor = '';
+        }
+    }
+    
 });
